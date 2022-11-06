9164bef6753651181d99f04754e5b50e19621a1f
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2022-11-06 20:44:30 +0100

    Add files via upload

---
+++ ROK/ROK/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ROK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj rok");
+            Decimal rok = decimal.Parse(Console.ReadLine()!);
+
+            if ((rok % 4 == 0) & (rok % 100 != 0))
+                Console.WriteLine(" Rok przestepny ");
+
+            else if (rok % 400 == 0)
+                Console.WriteLine(" Rok przestepny ");
+
+            else
+                Console.WriteLine(" Rok nie jest przestepny ");
+        }
+    }
+}
